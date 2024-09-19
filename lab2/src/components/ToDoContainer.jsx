6970902129769b2ce8b109aa2ae71fdb6532a2fb
VieronikaOsheiko <123954371+VieronikaOsheiko@users.noreply.github.com>
2024-09-20 01:14:17 +0300

    laba3fixcod

--- lab2/src/components/ToDoContainer.jsx
+++ lab2/src/components/ToDoContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { v4 } from "uuid";
 import SearchInput from './SearchInput';
-import AddToDOComponent from './AddToDOComponent';
+import AddToDoComponent from './AddToDoComponent';
 import ToDoTable from './ToDoTable';
 import ToDoEdit from './ToDoEdit';
 
@@ -57,7 +57,7 @@ const ToDoContainer = () => {
   return (
     <>
       <SearchInput searchTerm={searchTerm} onSearchChange={handleSearchChange} />
-      <AddToDOComponent
+      <AddToDoComponent
         title={newToDo?.title}
         onTitleChange={handleNewTitleChange}
         onSubmit={handleSubmit}
